7c7371d34127dffb951640fe04872b5aad0868c6
Leandro Rizzo <86168414+LeandroRizzo@users.noreply.github.com>
2021-06-19 17:02:59 -0300

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,9 @@
+
+let numero = parseInt(prompt("Ingrese un numero"));
+console.log (numero)    
+
+    if((numero >= 10) && (numero <= 50)){
+        console.log("Tu número está aprobado");
+    } else {
+        console.log("Ingrese otro número")
+    }
